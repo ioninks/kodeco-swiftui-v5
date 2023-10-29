285d2f919bfc1952abc938bb550be684fb8ecd2a
Konstantin Ionin <int4@me.com>
2023-10-29 14:07:51 +0100

    Chapter 5 finished

--- 05-intro-to-controls-text-and-image/Shared/Welcome/HorizontallyAlignedLabelStyle.swift
+++ 05-intro-to-controls-text-and-image/Shared/Welcome/HorizontallyAlignedLabelStyle.swift
@@ -32,17 +32,13 @@
 
 import SwiftUI
 
-@main
-struct KuchiApp: App {
-  var body: some Scene {
-    WindowGroup {
-      WelcomeView()
+struct HorizontallyAlignedLabelStyle: LabelStyle {
+  
+  func makeBody(configuration: Configuration) -> some View {
+    HStack {
+      configuration.icon
+      configuration.title
     }
   }
-}
-
-struct KuchiApp_Previews: PreviewProvider {
-  static var previews: some View {
-    WelcomeView()
-  }
+  
 }
--- 05-intro-to-controls-text-and-image/Shared/Welcome/WelcomeView.swift
+++ 05-intro-to-controls-text-and-image/Shared/Welcome/WelcomeView.swift
@@ -32,17 +32,47 @@
 
 import SwiftUI
 
-@main
-struct KuchiApp: App {
-  var body: some Scene {
-    WindowGroup {
-      WelcomeView()
+struct WelcomeView: View {
+  var body: some View {
+    ZStack {
+      Image("welcome-background", bundle: nil)
+        .resizable()
+        .aspectRatio(1 / 1, contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+        .ignoresSafeArea()
+        .saturation(0.5)
+        .blur(radius: 5)
+        .opacity(0.08)
+      
+      Label(
+        title: {
+          VStack(alignment: .leading) {
+            Text("Welcome to")
+              .font(.headline)
+              .bold()
+            
+            Text("Kuchi")
+              .font(.largeTitle)
+              .bold()
+          }
+          .foregroundStyle(.red)
+          .lineLimit(1)
+          .padding(.horizontal)
+        },
+        icon: {
+          Image(systemName: "table")
+            .resizable()
+            .frame(width: 30, height: 30)
+            .overlay(Circle().stroke(Color.gray, lineWidth: 1))
+            .background { Color(white: 0.9) }
+            .clipShape(Circle())
+            .foregroundStyle(.red)
+        }
+      )
+      .labelStyle(HorizontallyAlignedLabelStyle())
     }
   }
 }
 
-struct KuchiApp_Previews: PreviewProvider {
-  static var previews: some View {
-    WelcomeView()
-  }
+#Preview {
+  WelcomeView()
 }
