07ea6c4b8cad8ce789cf7ee42ef80dfe13ea674b
Konstantin Ionin <int4@me.com>
2023-10-31 14:44:56 +0100

    Chapter 9 finished

--- 09-state-and-data-flow-part-2/Shared/Practice/ChallengeView.swift
+++ 09-state-and-data-flow-part-2/Shared/Practice/ChallengeView.swift
@@ -36,23 +36,51 @@ struct ChallengeView: View {
   let challengeTest: ChallengeTest
   
   @State var showAnswers = false
+  @Binding var numberOfAnswered: Int
+  @Environment(\.verticalSizeClass) var verticalSizeClass
+  @Environment(\.questionsPerSession) var questionsPerSession
   
   var body: some View {
-    VStack {
-      Button(action: {
-        showAnswers.toggle()
-      }) {
-        QuestionView(question: challengeTest.challenge.question)
-          .frame(height: 300)
+    if verticalSizeClass == .compact {
+      VStack {
+        HStack {
+          Button(action: {
+            showAnswers.toggle()
+          }) {
+            QuestionView(question: challengeTest.challenge.question)
+          }
+                  
+          if showAnswers {
+            Divider()
+            ChoicesView(challengeTest: challengeTest)
+          }
+        }
+        
+        ScoreView(
+          numberOfQuestions: questionsPerSession,
+          numberOfAnswered: $numberOfAnswered
+        )
       }
-      
-      ScoreView(numberOfQuestions: 5)
-      
-      if showAnswers {
-        Divider()
-        ChoicesView(challengeTest: challengeTest)
-          .frame(height: 300)
-          .padding()
+    } else {
+      VStack {
+        Button(action: {
+          showAnswers.toggle()
+        }) {
+          QuestionView(question: challengeTest.challenge.question)
+            .frame(height: 300)
+        }
+        
+        ScoreView(
+          numberOfQuestions: questionsPerSession,
+          numberOfAnswered: $numberOfAnswered
+        )
+        
+        if showAnswers {
+          Divider()
+          ChoicesView(challengeTest: challengeTest)
+            .frame(height: 300)
+            .padding()
+        }
       }
     }
   }
@@ -69,7 +97,12 @@ struct ChallengeView_Previews: PreviewProvider {
     answers: ["Thank you", "Hello", "Goodbye"]
   )
   
+  @State static var numberOfAnswered: Int = 0
+  
   static var previews: some View {
-    return ChallengeView(challengeTest: challengeTest)
+    return ChallengeView(
+      challengeTest: challengeTest,
+      numberOfAnswered: $numberOfAnswered
+    )
   }
 }
--- 09-state-and-data-flow-part-2/Shared/Practice/ChallengesViewModel.swift
+++ 09-state-and-data-flow-part-2/Shared/Practice/ChallengesViewModel.swift
@@ -33,6 +33,19 @@
 import SwiftUI
 import Combine
 
+struct QuestionsPerSessionKey: EnvironmentKey {
+  static let defaultValue: Int = 5
+}
+
+extension EnvironmentValues {
+  
+  var questionsPerSession: Int {
+    get { self[QuestionsPerSessionKey.self] }
+    set { self[QuestionsPerSessionKey.self] = newValue }
+  }
+  
+}
+
 struct Challenge {
   let question: String
   let pronunciation: String
@@ -72,6 +85,7 @@ class ChallengesViewModel: ObservableObject {
   var correctAnswers: [Challenge] = []
   var wrongAnswers: [Challenge] = []
   
+  private(set) var numberOfQuestions = 6
   var numberOfAnswered: Int { return correctAnswers.count }
   @Published var currentChallenge: ChallengeTest?
   
@@ -98,7 +112,7 @@ class ChallengesViewModel: ObservableObject {
   }
   
   func generateRandomChallenge() {
-    if correctAnswers.count < 5 {
+    if correctAnswers.count < numberOfQuestions {
       currentChallenge = getRandomChallenge()
     } else {
       currentChallenge = nil
